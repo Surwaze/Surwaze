87fb455cfb736ae710c3dccde8c2de3ed6675044
livin <livinmathew99@gmail.com>
2017-02-03 17:43:31 +0530

    SurView Inflation and callbacks

--- SDK/src/main/java/com/lmntrx/surwaze/SurView.java
+++ SDK/src/main/java/com/lmntrx/surwaze/SurView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /***
  * Created by livin on 3/2/17.
@@ -12,12 +14,18 @@ import android.widget.RelativeLayout;
 
 public class SurView extends RelativeLayout {
 
+    Context context;
+
+    TextView sampleTV;
+
     public interface Callback{
         void onError(SurwazeException exception);
         void onComplete();
         void onSkipped();
     }
-    
+
+    Callback callbacks;
+
     public SurView(Context context) {
         super(context);
     }
@@ -34,4 +42,21 @@ public class SurView extends RelativeLayout {
     public SurView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes){
+        this.context = context;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.surview,this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        sampleTV = (TextView) findViewById(R.id.sampleTV);
+    }
+
+    public void setCallbacks(Callback callbacks){
+        this.callbacks = callbacks;
+    }
+
 }
